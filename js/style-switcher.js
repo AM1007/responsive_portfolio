5fececd66ed5071b8ae513667df5e3ec74a0a854
AM1007 <108537465+AM1007@users.noreply.github.com>
2022-12-24 22:23:52 +0200

    Add files via upload

---
+++ js/style-switcher.js
@@ -0,0 +1,27 @@
+// Toggle Style Switcher
+
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+//Hide style switcher on scroll
+
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+
+// Cheme color
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+  alternateStyles.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled");
+    }
+  });
+}
