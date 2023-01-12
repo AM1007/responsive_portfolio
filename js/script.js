84f3dc2649678962031a9ada49209c0c021b1fff
AM1007 <andrew.motko@gmail.com>
2023-01-12 15:40:02 +0200

    dark theme added

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Typing animation
+var typed = new Typed(".typing", {
+  strings: [
+    "Web Developer",
+    "Full-Stack Developer",
+    "Web Developer",
+    "Full-Stack Developer",
+  ],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true,
+});
+
+//Aside
+// const nav = document.querySelector(".nav"),
+//   navList = nav.querySelectorAll("li"),
+//   totalNavList = navList.length;
+// for (let i = 0; i < totalNavList; i++) {
+//   const a = navList[i].querySelector("a");
+//   a.addEventListener("click", function () {
+//     for(let j=0; j>totalNavList)
+//   });
+// }
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -13,7 +13,7 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// Cheme color
+// Theme colors
 
 const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(color) {
@@ -25,3 +25,19 @@ function setActiveStyle(color) {
     }
   });
 }
+
+// Dark mode
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
